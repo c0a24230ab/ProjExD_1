0f224629e525f9dfe5e56999fc0c55af3bbfda04
c0a24230ab <c0a24230ab@edu.teu.ac.jp>
2025-04-15 17:49:38 +0900

    練習１０

--- pygame_image.py
+++ pygame_image.py
@@ -14,6 +14,10 @@ def main():
     kk_img = pg.transform.flip(kk_img, True, False)# 練習８
     bg_img_flip = pg.transform.flip(bg_img, True,False)
     tmr = 0
+
+    kk_rct = kk_img.get_rect() #練習１０ー１
+    kk_rct.center = 300,200 #練習１０－２   
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return #練習３
@@ -22,13 +26,24 @@ def main():
         screen.blit(bg_img,[-x, 0])
         screen.blit(bg_img_flip,[-x+1600,0]) #練習７
         screen.blit(bg_img,[-x+3200,0])
-        screen.blit(kk_img,[300,200]) #練習４
 
-        kk_rct = kk_img.get_rect()
-        kk_rct.center = 300,200
 
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            kk_rct.move_ip((0,-1))
+
+        if key_lst[pg.K_DOWN]:
+            kk_rct.move_ip((0,1))
+
+        if key_lst[pg.K_LEFT]:
+            kk_rct.move_ip((-1,0))
+
+        if key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip((1,0))
+        screen.blit(kk_img,kk_rct) #練習４
+        
         pg.display.update()
-        tmr += 1        
+        tmr += 1
         clock.tick(200) #練習５
 
 
